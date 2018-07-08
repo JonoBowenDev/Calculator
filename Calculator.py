975096a47f831726a439a3be2bbd9b18e3dc39ab
jono1234321 <38906825+jono1234321@users.noreply.github.com>
2018-07-08 23:27:35 +1000

    Add Padding Variables to Help Adjust the Grid
    - Add Padding Variables
    - Add Padding for all of the buttons

--- Calculator.py
+++ Calculator.py
@@ -91,91 +91,110 @@ display = Label(the_window, text = '0',
                 font = calc_font)
 display.grid(row = 0, column = 3)
 
-# Create the numbers
+# Set some variables for easy grid adjustment 
+x_button_padding = 10
+y_button_padding = 10
+
+# Create the number buttons
 number_1 = Button(the_window, text = "1",
                   height = 2, width = 4,
                   font = calc_font, command = no_1) 
-number_1.grid(row = 1, column = 0, padx = 2, pady = 2) 
+number_1.grid(row = 1, column = 0,
+              padx = x_button_padding, pady = y_button_padding) 
 
 number_2 = Button(the_window, text = "2",
                   height = 2, width = 4,
                   font = calc_font, command = no_2)
-number_2.grid(row = 1, column = 1) 
+number_2.grid(row = 1, column = 1,
+              padx = x_button_padding, pady = y_button_padding) 
 
 number_3 = Button(the_window, text = "3",
                   height = 2, width = 4,
                   font = calc_font, command = no_3) 
-number_3.grid(row = 1, column = 2)
+number_3.grid(row = 1, column = 2,
+              padx = x_button_padding, pady = y_button_padding)
 
 number_4 = Button(the_window, text = "4",
                   height = 2, width = 4,
                   font = calc_font, command = no_4) 
-number_4.grid(row = 2, column = 0)
+number_4.grid(row = 2, column = 0,
+              padx = x_button_padding, pady = y_button_padding)
 
 number_5 = Button(the_window, text = "5",
                   height = 2, width = 4,
                   font = calc_font, command = no_5) 
-number_5.grid(row = 2, column = 1)
+number_5.grid(row = 2, column = 1,
+              padx = x_button_padding, pady = y_button_padding)
 
 number_6 = Button(the_window, text = "6",
                   height = 2, width = 4,
                   font = calc_font, command = no_6) 
-number_6.grid(row = 2, column = 2)
+number_6.grid(row = 2, column = 2,
+              padx = x_button_padding, pady = y_button_padding)
 
 number_7 = Button(the_window, text = "7",
                   height = 2, width = 4,
                   font = calc_font, command = no_7) 
-number_7.grid(row = 3, column = 0)
+number_7.grid(row = 3, column = 0,
+              padx = x_button_padding, pady = y_button_padding)
 
 number_8 = Button(the_window, text = "8",
                   height = 2, width = 4,
                   font = calc_font, command = no_8) 
-number_8.grid(row = 3, column = 1)
+number_8.grid(row = 3, column = 1,
+              padx = x_button_padding, pady = y_button_padding)
 
 number_9 = Button(the_window, text = "9",
                   height = 2, width = 4,
                   font = calc_font, command = no_9) 
-number_9.grid(row = 3, column = 2)
+number_9.grid(row = 3, column = 2,
+              padx = x_button_padding, pady = y_button_padding)
 
 number_0 = Button(the_window, text = '0',
                   height = 2, width = 4,
                   font = calc_font, command = no_0) 
-number_0.grid(row = 4, column = 1)
+number_0.grid(row = 4, column = 1,
+              padx = x_button_padding, pady = y_button_padding)
 
 # Create the symbols (+, -, *, /, =, c,)
 plus = Button(the_window, text = '+',
               height = 2, width = 4,
               font = calc_font, command = addition)
-plus.grid(row = 1, column = 3)
+plus.grid(row = 1, column = 3,
+          padx = x_button_padding, pady = y_button_padding)
 
 minus = Button(the_window, text = '-',
               height = 2, width = 4,
               font = calc_font, command = subtraction)
-minus.grid(row = 2, column = 3) 
+minus.grid(row = 2, column = 3,
+           padx = x_button_padding, pady = y_button_padding)
 
 multiply = Button(the_window, text = '*',
               height = 2, width = 4,
               font = calc_font, command = multiplication)
-multiply.grid(row = 3, column = 3) 
+multiply.grid(row = 3, column = 3,
+              padx = x_button_padding, pady = y_button_padding)
 
 divide = Button(the_window, text = '/',
               height = 2, width = 4,
               font = calc_font, command = dividation) 
-divide.grid(row = 4, column = 3) 
+divide.grid(row = 4, column = 3,
+            padx = x_button_padding, pady = y_button_padding)
 
 equate = Button(the_window, text = '=',
               height = 2, width = 4,
               font = calc_font, command = equation)
-equate.grid(row = 4, column = 2)
+equate.grid(row = 4, column = 2,
+            padx = x_button_padding, pady = y_button_padding)
 
 reset = Button(the_window, text = 'c',
                height = 2, width = 4,
                font = calc_font, command = clear)
-reset.grid(row = 4, column = 0)
+reset.grid(row = 4, column = 0,
+           padx = x_button_padding, pady = y_button_padding)
 
 # Start the events loop 
 mainloop()
-
 
 
 
